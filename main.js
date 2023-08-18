22483e20b073fbb3d23f91e824cad896c5944933
ESTELA ALVES DE MORAES <23111781@CFP127ALUNO.LOCAL>
2023-08-18 16:11:08 -0300

    exercício d funcionando

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const botaoIdentificar = document.getElementById('identificar')
 function somar(){
     const numero1 = Number(document.getElementById('numero1').value)
     const numero2 = Number(document.getElementById('numero2').value)
-    const total = numero1 + numero2;
+    const total = numero1 + numero2
 
     const resultado = document.getElementById('resultado')
-    resultado.value = total
+    resultado.textContent = total
 }
 
 function identificar(){
@@ -27,3 +27,56 @@ function identificar(){
 
 botaoSomar.addEventListener('click', somar)
 botaoIdentificar.addEventListener('click', identificar)
+
+////////////////////////////////////////////////////////////
+///questão d
+const botaoMedia = document.getElementById('avaliar')
+
+function media(){
+
+    const n1 = Number(document.getElementById('nota1').value)
+    const n2 = Number(document.getElementById('nota2').value)
+    const n3 = Number(document.getElementById('nota3').value)
+    const n4 = Number(document.getElementById('nota4').value)
+    const n5 = Number(document.getElementById('nota5').value)
+
+    const mediaResultado = document.getElementById('media')
+    const status = document.getElementById('status')
+    const notaExame = document.getElementById('nota5')
+    const exameDiv = document.getElementById('div_exame')
+
+    if(botaoMedia.textContent === 'Reavaliar'){
+
+        const resultado = (n1+n2+n3+n4+n5)/5
+        mediaResultado.textContent = resultado
+        
+        if(resultado < 7){
+            status.textContent = 'Reprovado'
+            botaoMedia.textContent = 'Avaliar'
+        }else{
+            status.textContent = 'Aprovado por exame!'
+        }
+
+    }else{
+
+        const resultado = (n1+n2+n3+n4)/4
+        mediaResultado.textContent = resultado
+
+        if(resultado < 7){
+            status.textContent = 'Em exame'
+            exameDiv.classList.remove('exame')
+            botaoMedia.textContent = 'Reavaliar'
+        }else{
+            status.textContent = 'Aprovado!'
+            exameDiv.classList.add('exame')
+        }
+        
+    }
+  
+}
+
+botaoMedia.addEventListener('click', media)
+
+//////////////////////////////////
+////////questão f
+
